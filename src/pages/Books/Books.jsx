31bac913966264c89fff60331edf0dfbb558602a
Mintu Sikder <mintusikder15@gmail.com>
2025-04-17 14:21:49 +0600

    added card data

---
+++ src/pages/Books/Books.jsx
@@ -0,0 +1,28 @@
+import React, { Suspense, useEffect, useState } from "react";
+import Book from "../Book/Book";
+
+const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("../../../public/booksData.json")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
+  return (
+    <div>
+      <h3 className="text-center text-3xl font-bold mb-6">Book</h3>
+      <div>
+        <Suspense fallback={<p>Loading....</p>}>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {books.map((book) => (
+              <Book book={book} key={book.id}></Book>
+            ))}
+          </div>
+        </Suspense>
+      </div>
+    </div>
+  );
+};
+
+export default Books;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import Banner from '../../components/Banner/Banner';
+import React from "react";
+import Banner from "../../components/Banner/Banner";
+import Books from "../Books/Books";
 
 const Home = () => {
-    return (
-        <div>
-            <div className='py-12'>
-            <Banner></Banner>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="py-12">
+        <Banner></Banner>
+      </div>
+      <Books></Books>
+    </div>
+  );
 };
 
 export default Home;
